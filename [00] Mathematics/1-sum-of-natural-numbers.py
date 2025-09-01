37b20b56125faef8d240b18b51a74f2f8f226163
Swinalwaghmare <swinalwaghmare28.com>
2025-09-01 09:58:22 +0530

    ✨ Added program to calculate sum of N natural numbers ➕🔢

--- [00] Mathematics/1-sum-of-natural-numbers.py
+++ [00] Mathematics/1-sum-of-natural-numbers.py
@@ -5,6 +5,7 @@ class Solution:
     def sumofnaturalnums(self):
         print(self.n * (self.n + 1)//2)
 
+        
 if __name__ == '__main__':
     n = int(input("Enter a Number: "))
     obj = Solution(n)
