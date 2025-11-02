c36359cedf823a4aee10d611b3b1ee327f182f31
Swinalwaghmare <swinalwaghmare28.com>
2025-11-02 12:39:44 +0530

    recursion day 5

--- [02] Data Structure (Basic)/[03] Recursion/problem6-num-palindrome.py
+++ [02] Data Structure (Basic)/[03] Recursion/problem6-num-palindrome.py
@@ -9,5 +9,23 @@
 # n = 101
 # Output: 1
 
-n = 100
-print(n[1])
+class Solution:
+    def reverse_num(self, n, rev=0):
+        if n == 0:
+            return rev
+        last_digit = n % 10
+        rev = rev * 10 + last_digit
+        
+        return self.reverse_num(n // 10, rev)
+        
+    def isPalin(self,N):
+        if N == self.reverse_num(N):
+            return 1
+        else:
+            return 0
+
+if __name__ == '__main__':
+    obj = Solution()
+    N = int(input("Enter a Number: "))
+    result = obj.isPalin(N)
+    print(result)
